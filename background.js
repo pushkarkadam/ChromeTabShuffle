78fbc7697d9f8a85b482e6e810a146741a6e545f
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-02-18 16:37:25 +1100

    Content script removed.

--- background.js
+++ background.js
@@ -2,13 +2,19 @@ var date = new Date();
 var hour = date.getHours()
 var START_TIME = 9;
 var END_TIME = 16;
+var tabs = {};
 
 chrome.runtime.onInstalled.addListener(() => {
-        if (hour < START_TIME && hour > END_TIME) {
-            // TODO: Go to tab #2
-        }
-        else {
-            // TODO: Go to tab #1
-        }
-    });
+    if (hour < START_TIME && hour > END_TIME) {
+        // TODO: Go to tab #2
+        chrome.tabs.goForward(chrome.tabs.query({"windowId": windowId}), function(tab) {
+            chrome.tabs.reload(chrome.tabs.query({"windowId": windowId}));
+        });
+    }
+    else {
+        // TODO: Go to tab #1
+        chrome.tabs.goBack(chrome.tabs.query({"windowId": windowId - 1}), function(tab) {
+            chrome.tabs.reload(chrome.tabs.query({"windowId": windowId - 1}));
+        });
+    }
 });
