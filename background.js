b4c9bd58c24ee7f81a31d8ad392839cbda250aef
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-02-18 11:42:35 +1100

    runtime.onInstalled Event Listener added.

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 var date = new Date();
 var hour = date.getHours()
+var START_TIME = 9;
+var END_TIME = 16;
 
-if (hour < 9 && hour > 17) {
-    // TODO: Go to Tab #2
-    chrome.tabs.goForward(2);
-}
-
-else {
-    // TODO: Go to Tab #1
-    chrome.tabs.goBack(1);
-}
+chrome.runtime.onInstalled.addListener(() => {
+        if (hour < START_TIME && hour > END_TIME) {
+            // TODO: Go to tab #2
+        }
+        else {
+            // TODO: Go to tab #1
+        }
+    });
+});
