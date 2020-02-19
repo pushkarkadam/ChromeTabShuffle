a6177aec30ee457bde1ba98e919fd74496469a1c
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-02-19 15:36:29 +1100

    Array of tabs created to store tab id of all open tabs.

--- background.js
+++ background.js
@@ -2,10 +2,11 @@ var date = new Date();
 var hour = date.getHours()
 var START_TIME = 9;
 var END_TIME = 16;
-var tabs = {};
+var tabs_array = [];
 
 chrome.tabs.query({}, function(tabs) {
     for (var i = 0; i < tabs.length; i++) {
-        console.log(tabs[i].id);
+        tabs_array.push(tabs[i].id);
     }
+    console.log(tabs_array);
 });
