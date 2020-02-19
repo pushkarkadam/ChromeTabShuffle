041afc4fab4d3656a7dceeeae47573f8f9ab1800
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-02-19 15:31:40 +1100

    Tabs id obtained.

--- background.js
+++ background.js
@@ -4,17 +4,8 @@ var START_TIME = 9;
 var END_TIME = 16;
 var tabs = {};
 
-chrome.runtime.onInstalled.addListener(() => {
-    if (hour < START_TIME && hour > END_TIME) {
-        // TODO: Go to tab #2
-        chrome.tabs.goForward(chrome.tabs.query({"windowId": windowId}), function(tab) {
-            chrome.tabs.reload(chrome.tabs.query({"windowId": windowId}));
-        });
-    }
-    else {
-        // TODO: Go to tab #1
-        chrome.tabs.goBack(chrome.tabs.query({"windowId": windowId - 1}), function(tab) {
-            chrome.tabs.reload(chrome.tabs.query({"windowId": windowId - 1}));
-        });
+chrome.tabs.query({}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        console.log(tabs[i].id);
     }
 });
