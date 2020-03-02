a1528b48aa4e33c2a3a4fe3e2c47d7664c1ecc9e
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-02 12:59:55 +1100

    Hour conditions working.

--- background.js
+++ background.js
@@ -8,5 +8,23 @@ chrome.tabs.query({}, function(tabs) {
     for (var i = 0; i < tabs.length; i++) {
         tabs_array.push(tabs[i].id);
     }
+    // Display array of tabs
     console.log(tabs_array);
+
+    // Check the hour of the day
+    if (hour < START_TIME && hour > END_TIME) {
+        chrome.tabs.query({active: true}, function(tabs) {
+            chrome.tabs.reload(tabs_array[1]);
+            console.log("executed tab 1");
+        });
+    }
+    else {
+        chrome.tabs.query({active: true}, function(tabs) {
+            chrome.tabs.query({windowId: tabs_array[0]}, function(tabs) {
+                chrome.tabs.reload(tabs_array[0]);
+                console.log("executed tab 0");
+            });
+        });
+    }
+
 });
