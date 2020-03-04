e56d75ee787ad27c409b82b8fad6e04b028dc2e2
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-04 14:42:00 +1100

    Conditional time working.

--- background.js
+++ background.js
@@ -5,26 +5,26 @@ var END_TIME = 16;
 var tabs_array = [];
 
 chrome.tabs.query({}, function(tabs) {
+    console.log(tabs);
     for (var i = 0; i < tabs.length; i++) {
         tabs_array.push(tabs[i].id);
     }
     // Display array of tabs
     console.log(tabs_array);
+    console.log("Hour: " + hour.toString());
 
     // Check the hour of the day
-    if (hour < START_TIME && hour > END_TIME) {
-        chrome.tabs.query({active: true}, function(tabs) {
-            chrome.tabs.reload(tabs_array[1]);
-            console.log("executed tab 1");
+    if (hour < START_TIME || hour > END_TIME) {
+        chrome.tabs.query({"windowId": tabs_array[1], "active": true}, function(tabs) {
+            // chrome.tabs.highlight
+            chrome.tabs.update(tabs_array[1], {"highlighted": true});
+            console.log("executed tab ID " + tabs_array[1].toString());
         });
     }
     else {
-        chrome.tabs.query({active: true}, function(tabs) {
-            chrome.tabs.query({windowId: tabs_array[0]}, function(tabs) {
-                chrome.tabs.reload(tabs_array[0]);
-                console.log("executed tab 0");
-            });
+        chrome.tabs.query({"windowId": tabs_array[0], "active": true}, function(tabs) {
+            chrome.tabs.reload(tabs.id);
+            console.log("executed tab ID " + tabs_array[0].toString());
         });
     }
-
 });
