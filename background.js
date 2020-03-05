b6a25823bd98dedc5250e61d420bf264ec404660
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-05 16:05:45 +1100

    Popup.html added with submit button.

--- background.js
+++ background.js
@@ -15,16 +15,16 @@ chrome.tabs.query({}, function(tabs) {
 
     // Check the hour of the day
     if (hour < START_TIME || hour > END_TIME) {
-        chrome.tabs.query({"windowId": tabs_array[1], "active": true}, function(tabs) {
+        chrome.tabs.query({"windowId": tabs[1].windowId, "active": true}, function(tab) {
             // chrome.tabs.highlight
             chrome.tabs.update(tabs_array[1], {"highlighted": true});
             console.log("executed tab ID " + tabs_array[1].toString());
         });
     }
     else {
-        chrome.tabs.query({"windowId": tabs_array[0], "active": true}, function(tabs) {
-            chrome.tabs.reload(tabs.id);
-            console.log("executed tab ID " + tabs_array[0].toString());
+        chrome.tabs.query({"windowId": tabs[0].windowId, "active": true}, function(tab) {
+            chrome.tabs.reload(tab.id);
+            console.log("executed tab ID " + tabs[0].id.toString());
         });
     }
 });
