64750885668bc5519c729ae9101502a03e0ff89e
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-11 17:20:56 +1100

    Tab shuffling working as per time.

--- background.js
+++ background.js
@@ -3,28 +3,30 @@ var hour = date.getHours()
 var START_TIME = 9;
 var END_TIME = 16;
 var tabs_array = [];
+var tabs_manifest = [];
 
-chrome.tabs.query({}, function(tabs) {
-    console.log(tabs);
-    for (var i = 0; i < tabs.length; i++) {
-        tabs_array.push(tabs[i].id);
-    }
-    // Display array of tabs
-    console.log(tabs_array);
-    console.log("Hour: " + hour.toString());
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.query({}, function(tabs) {
+        console.log(tabs);
+        for (var i = 0; i < tabs.length; i++) {
+            tabs_array.push(tabs[i].index);
+            tabs_manifest.push(tabs[i]);
+        }
+        // Display array of tabs
+        console.log(tabs_array);
+        console.log(tabs_manifest);
+        console.log("Hour: " + hour.toString());
 
-    // Check the hour of the day
-    if (hour < START_TIME || hour > END_TIME) {
-        chrome.tabs.query({"windowId": tabs[1].windowId, "active": true}, function(tab) {
-            // chrome.tabs.highlight
-            chrome.tabs.update(tabs_array[1], {"highlighted": true});
-            console.log("executed tab ID " + tabs_array[1].toString());
-        });
-    }
-    else {
-        chrome.tabs.query({"windowId": tabs[0].windowId, "active": true}, function(tab) {
-            chrome.tabs.reload(tab.id);
-            console.log("executed tab ID " + tabs[0].id.toString());
-        });
-    }
+        // Check the hour of the day
+        if (hour > START_TIME && hour < END_TIME) {
+            chrome.tabs.move(tabs_manifest[0].id, {index: 0});
+            console.log(tabs_manifest[0].index);
+            console.log(tabs_manifest[0].url);
+        }
+        else {
+            chrome.tabs.move(tabs_manifest[1].id, {index: 0});
+            console.log("Index: " + tabs_manifest[1].index.toString());
+            console.log(tabs_manifest[1].url);
+        }
+    });
 });
