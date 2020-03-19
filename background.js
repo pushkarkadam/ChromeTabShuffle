2df034699e71532ff73ec7275d1ddc613340db5a
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-20 10:58:02 +1100

    tabs_array removed.

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 var date = new Date();
-var hour = date.getHours()
-var tabs_array = [];
+var hour = date.getHours();
 var tabs_manifest = [];
 
 // Loads config file
@@ -11,28 +10,21 @@ const END_TIME = config.end_time;
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.tabs.query({}, function(tabs) {
-        console.log(tabs);
         for (var i = 0; i < tabs.length; i++) {
-            tabs_array.push(tabs[i].index);
             tabs_manifest.push(tabs[i]);
         }
-        // Display array of tabs
-        console.log(tabs_array);
-        console.log(tabs_manifest);
-        console.log("Hour: " + hour.toString());
 
         // Check the hour of the day
         if (hour > START_TIME && hour < END_TIME) {
-            // go through the tabs_manifest to find the form link
+            // go through the tabs_manifest to find the feedback_link
             for (var i = 0; i < tabs_manifest.length; i++) {
                 if (tabs_manifest[i].url == config.feedback_link) {
                     chrome.tabs.move(tabs_manifest[i].id, {index: 0});
-                    console.log(tabs_manifest[i].url);
                 }
             }
         }
         else {
-            // go through the tabs_manifest to find the feedback_link
+            // go through the tabs_manifest to find the google_form
             for (var i = 0; i < tabs_manifest.lenght; i++) {
                 if (tabs_manifest[i].url == config.google_form) {
                     chrome.tabs.move(tabs_manifest[i].id, {index: 0});
