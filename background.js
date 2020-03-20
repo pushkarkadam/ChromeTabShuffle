9ff74007571dff64fbe6d105b8fae91bbf77d3c0
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-03-20 18:02:37 +1100

    Auto reload of extension added.

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
-var date = new Date();
-var hour = date.getHours();
-var tabs_manifest = [];
-
-// Loads config file
-var config = chrome.extension.getBackgroundPage().config;
-
-const START_TIME = config.start_time;
-const END_TIME = config.end_time;
+setInterval(function() {
+    chrome.runtime.reload();
+}, 1000 * 60);
 
 chrome.runtime.onInstalled.addListener(function() {
+    var date = new Date();
+    var hour = date.getHours();
+    var tabs_manifest = [];
+
+    // Loads config file
+    var config = chrome.extension.getBackgroundPage().config;
+
+    const START_TIME = config.start_time;
+    const END_TIME = config.end_time;
+
     chrome.tabs.query({}, function(tabs) {
         for (var i = 0; i < tabs.length; i++) {
             tabs_manifest.push(tabs[i]);
